751b7bb188e64770698e2687992bf1d00d212944
ken505 <68226398+ken505@users.noreply.github.com>
2021-08-10 12:26:21 +0900

    🔥 Rmove dead code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,14 +5,12 @@ import { Links } from "../components/Links";
 const Home = () => {
   return (
     <ContainerLayoutWrapper>
-      <main>
         <h1 className="text-xl flex justify-center mb-10 font-bold">
           Welcome to Ken&apos;s links
         </h1>
         <CardLayoutWrapper>
           <Links />
         </CardLayoutWrapper>
-      </main>
     </ContainerLayoutWrapper>
   );
 };
