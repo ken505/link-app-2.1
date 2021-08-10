27c39d917f25b1ea07ba8112422f4bcdfb36bbae
ken505 <68226398+ken505@users.noreply.github.com>
2021-08-11 01:08:42 +0900

    💄 Create layoutwrapper

---
+++ src/components/layout/LinkCardLayoutWrapper.jsx
@@ -0,0 +1,17 @@
+export const CardLayoutWrapper = (props) => {
+  return (
+    <div
+      className="mx-auto"
+    >
+      <div>{props.children}</div>
+    </div>
+  );
+};
+// colorsample
+// bg-gradient-to-tr
+// bg-gradient-radial-at-t
+// from-white via-white to-gray-300
+// from-pink-300 via-blue-300 to-green-200
+// dark:from-indigo-700 dark:via-purple-600 dark:to-pink-500
+
+// grid grid-rows-[auto,1fr,auto]
