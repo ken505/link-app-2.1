b814f7db02d838f00a4306fb82b948ef24889014
ken505 <68226398+ken505@users.noreply.github.com>
2021-08-11 10:23:54 +0900

    ⚰️ Remove dead code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,3 @@
-import { LinkCardLayoutWrapper } from "../components/Layout/LinkCardLayoutWrapper";
 import { ContainerLayoutWrapper } from "../components/Layout/ContainerLayoutWrapper";
 import { Links } from "../components/Link";
 
@@ -8,9 +7,7 @@ const Home = () => {
       <h1 className="text-xl flex justify-center mb-10 font-bold">
         Welcome to Ken&apos;s links
       </h1>
-      <LinkCardLayoutWrapper>
-        <Links />
-      </LinkCardLayoutWrapper>
+      <Links />
     </ContainerLayoutWrapper>
   );
 };
