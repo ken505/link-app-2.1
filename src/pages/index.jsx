5a9daa132104800f42b1717723901b8b930e341e
ken505 <68226398+ken505@users.noreply.github.com>
2021-08-11 10:21:24 +0900

    ✏️ Fix a pass name typo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,17 @@
-// import { CardLayoutWrapper } from "../components/Layout/LinkCardLayoutWrapper";
-// import { ContainerLayoutWrapper } from "../components/Layout/ContainerLayoutWrapper";
-// import { Links } from "../components/Link";
+import { LinkCardLayoutWrapper } from "../components/Layout/LinkCardLayoutWrapper";
+import { ContainerLayoutWrapper } from "../components/Layout/ContainerLayoutWrapper";
+import { Links } from "../components/Link";
 
-// const Home = () => {
-//   return (
-//     <ContainerLayoutWrapper>
-//       <h1 className="text-xl flex justify-center mb-10 font-bold">
-//         Welcome to Ken&apos;s links
-//       </h1>
-//       <CardLayoutWrapper>
-//         <Links />
-//       </CardLayoutWrapper>
-//     </ContainerLayoutWrapper>
-//   );
-// };
-// export default Home;
+const Home = () => {
+  return (
+    <ContainerLayoutWrapper>
+      <h1 className="text-xl flex justify-center mb-10 font-bold">
+        Welcome to Ken&apos;s links
+      </h1>
+      <LinkCardLayoutWrapper>
+        <Links />
+      </LinkCardLayoutWrapper>
+    </ContainerLayoutWrapper>
+  );
+};
+export default Home;
